61df2673be9a2fed11a74db3f421b1ff907139ac
Chris Collins <chris.collins.1210@gmail.com>
2018-06-01 21:14:38 -0700

    use gumbo parser and run simple test

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 CXX := clang++
-CXXFLAGS += -g -Wall -Wextra -std=c++17 -fsanitize=address
+CXXFLAGS += -g -Wall -Wextra -std=c++17 -fsanitize=address -I/usr/local/Cellar/gumbo-parser/0.10.1/include -L/usr/local/Cellar/gumbo-parser/0.10.1/lib -lgumbo
 
 b_tree: b_tree.cpp b_tree.h
 	$(CXX) $< -o $@ $(CXXFLAGS)
--- b_tree.cpp
+++ b_tree.cpp
@@ -1,32 +1,105 @@
 #include "b_tree.h"
+#include "gumbo.h"
+// static void test() {
+//   std::shared_ptr<Node<std::string, std::string, 20>> current_root;
+//   std::vector<std::shared_ptr<Node<std::string, std::string, 20>>> tree_history;
+//   std::vector<std::string> values;
+
+//   for (int i = 0; i < 500; i++) {
+//     values.push_back("v" + std::to_string(rand() % 100));
+//     std::string key = "k" + std::to_string(i);
+//     std::cout << "key: " << key << " value: " << values[i] << std::endl;
+//     current_root = Insert(current_root, std::make_pair(key, values[i]));
+//     tree_history.push_back(current_root);
+//   }
+
+//   for (int i = 0; i < 500; i++) {
+//     std::string key = "k" + std::to_string(i);
+//     // std::cout << (std::string) *Find(tree_history[i], key) << std::endl;
+//     assert((std::string) *Find(tree_history[i], key) == values[i]);
+//   }
+//   std::cout << "found all at relative root " << std::endl;
+
+//   for (int i = 0; i < 500; i++) {
+//     std::string key = "k" + std::to_string(i);
+//     assert((std::string)*Find(tree_history[499], key) == values[i]);
+//   }
+//   std::cout << "found all at current root " << std::endl;
+
+//   for (int i = 500; i < 600; i++) {
+//     std::string key = "k" + std::to_string(i);
+//     assert(Find(tree_history[499], key) == OPTIONAL_NS::nullopt);
+//   }
+//   std::cout << "no false positives" << std::endl;
+
+//   // remove 50 - 199 from latest version of the tree
+//   for (int i = 50; i < 200; i++) {
+//     std::string key = "k" + std::to_string(i);
+//     current_root = Remove(current_root, key);
+//   }
+
+//   // ensure that 50 - 199 aren't in latest version of tree
+//   for (int i = 50; i < 200; i++) {
+//     std::string key = "k" + std::to_string(i);
+//     assert(Find(current_root, key) == OPTIONAL_NS::nullopt);
+//   }
+//   std::cout << "no remove false positives" << std::endl;
+
+//   for (int i = 50; i < 200; i++) {
+//     std::string key = "k" + std::to_string(i);
+//     assert((std::string)*Find(tree_history[499], key) == values[i]);
+//   }
+//   std::cout << "no remove false negatives" << std::endl;
+// }
+
+
+static const char* find_title(const GumboNode* root) {
+  assert(root->type == GUMBO_NODE_ELEMENT);
+  assert(root->v.element.children.length >= 2);
+
+  const GumboVector* root_children = &root->v.element.children;
+  // GumboNode* head = NULL;
+  for (size_t i = 0; i < root_children->length; ++i) {
+    GumboNode* child = (GumboNode*)root_children->data[i];
+    std::cout << gumbo_normalized_tagname(child->v.element.tag) << ", # children: " << child->v.element.children.length << std::endl;
+    if (child->type == GUMBO_NODE_ELEMENT &&
+        child->v.element.tag == GUMBO_TAG_BODY) {
+        const GumboVector* body_children = &child->v.element.children;
+        for (size_t j = 0; j < body_children->length; j++) {
+          GumboNode* bodyChild = (GumboNode*)body_children->data[j];
+          // std::cout << bodyChild << std::endl;
+          std::cout << gumbo_normalized_tagname(bodyChild->v.element.tag);
+          GumboNode* c = (GumboNode*)bodyChild->v.element.children.data[0];
+          std::cout << " " << (c->type == GUMBO_NODE_TEXT) << " " << c->v.text.text << std::endl;
+        }
+    }
+    // if (child->type == GUMBO_NODE_ELEMENT &&
+    //     child->v.element.tag == GUMBO_TAG_HEAD) {
+    //   head = child;
+    //   break;
+    // }
+  }
+  // assert(head != NULL);
+
+  // GumboVector* head_children = &head->v.element.children;
+  // for (size_t i = 0; i < head_children->length; ++i) {
+  //   GumboNode* child = (GumboNode*)head_children->data[i];
+  //   if (child->type == GUMBO_NODE_ELEMENT &&
+  //       child->v.element.tag == GUMBO_TAG_TITLE) {
+  //     if (child->v.element.children.length != 1) {
+  //       return "<empty title>";
+  //     }
+  //     GumboNode* title_text = (GumboNode*)child->v.element.children.data[0];
+  //     assert(title_text->type == GUMBO_NODE_TEXT || title_text->type == GUMBO_NODE_WHITESPACE);
+  //     return title_text->v.text.text;
+  //   }
+  // }
+  return "<no title found>";
+}
 
 int main() {
-  std::default_random_engine engine{std::random_device{}()};
-  std::uniform_int_distribution<> distribution{0, 127};
-  std::shared_ptr<Node<int, int, 2>> root;
-  std::vector<std::shared_ptr<Node<int, int, 2>>> roots;
-  for (int i = 0; i < 90; ++i) {
-    root = Insert(root, std::make_pair(i, i * 100));
-  }
-  root = Remove(root, 88);
-  root = Remove(root, 89);
-  root = Remove(root, 84);
-  root = Remove(root, 87);
-  root = Remove(root, 83);
-  root = Remove(root, 86);
-  root = Remove(root, 85);
-  for (int i = 0; i < 80; ++i) {
-    assert(*Find(root, i) == i * 100);
-    root = Remove(root, i);
-    roots.push_back(root);
-  }
-  root = Remove(root, 79);
-  root = Remove(root, 80);
-  root = Remove(root, 81);
-  root = Remove(root, 82);
-
-  for (auto&& i : roots) {
-    std::cout << ToDot(i) << std::endl;
-  }
+  GumboOutput* output = gumbo_parse("<h1>Hello, World!</h1>");
+  std::cout << find_title(output->root) << std::endl;
+  gumbo_destroy_output(&kGumboDefaultOptions, output);
   return 0;
 }
